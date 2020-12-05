3bc532b76b0a0dea21b7359f116a033e107652bb
Moad Naciri <63821666+moadnaciri@users.noreply.github.com>
2020-12-05 18:20:16 +0100

    main

--- src/components/Error.js
+++ src/components/Error.js
@@ -7,17 +7,17 @@ import './Hero.css'
 
 function Error() {
     return (
-        <HeroContainer>
-            <HeroBg>
-                <Img src={Image}/>
-            </HeroBg>
-            <HeroContent>
-                <HeroItems>
-                    <HeroH1>404: Not Found</HeroH1>
-                    <HeroP>The page you are looking for no longer exists. Click <Link to='/' style={{ textDecoration: 'none', color: '#F26A2E' }}>here</Link> to go to the home page.</HeroP>
-                </HeroItems>
-            </HeroContent>
-        </HeroContainer>
+        <div className='HeroContainer'>
+            <div className='HeroBg'>
+                <video className='VideoBg' src={Video} type='video/mp4' autoPlay loop muted playsInline />
+            </div>
+            <div className='HeroContent'>
+                <div className='HeroItems'>
+                    <h1 className='HeroH1'>404: Not Found</h1>
+                    <p className='HeroP'>The page you are looking for no longer exists. Click <Link to='/' style={{ textDecoration: 'none', color: '#F26A2E' }}>here</Link> to go to the home page.</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
